f35f7266e66cd7556f40dd3870e7ba2df3bda6c2
Sushant <shekhar.sushant@rabobank.nl>
2020-11-22 00:07:21 +0100

    array operations

--- Datastructure/src.java.datastructure/src/com/practice/collections/array/InsertDuplicateZeros.java
+++ Datastructure/src.java.datastructure/src/com/practice/collections/array/InsertDuplicateZeros.java
@@ -1,4 +1,27 @@
 package com.practice.collections.array;
 
+import java.util.Arrays;
+
 public class InsertDuplicateZeros {
+    public static void main(String[] args) {
+        int[] A = {1,0,2,3,0,4,5,0};
+        duplicateZeros(A);
+    }
+    public static void duplicateZeros(int[] arr) {
+        for(int x=0;x<arr.length;x++){
+            try{
+                if(arr[x] == 0){
+//                    System.out.println("index = "+ x);
+                    for (int i = arr.length-2; i >= x; i--) {
+                        arr[i + 1] = arr[i];
+//                        System.out.println(" -> "+ Arrays.toString(arr));
+                    }
+                    x+= 1;
+                }
+            }catch(ArrayIndexOutOfBoundsException ex){
+                continue;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
--- Datastructure/src.java.datastructure/src/com/practice/collections/array/MergeSortedArray.java
+++ Datastructure/src.java.datastructure/src/com/practice/collections/array/MergeSortedArray.java
@@ -1,2 +1,44 @@
-package com.practice.collections.array;public class MergeSortedArray {
+package com.practice.collections.array;
+
+import java.util.Arrays;
+
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] A ={1,2,3,0,0,0};
+        int lenA= A.length;
+        int[] B ={2,5,6};
+        int lenB= B.length;
+        mergeArray(A, lenA, B, lenB);
+    }
+
+    private static void mergeArray(int[] a, int m, int[] b, int n) {
+        int x= m-1;
+        int y = n-1;
+
+        int[] f = a.clone();
+        while(n !=0){
+            --m;
+            a[m]= b[y];
+            y--;
+            n--;
+        }
+        Arrays.sort(a);
+
+        System.out.println(Arrays.toString(a));
+    }
+
+    // Second solution in line with LeetCode
+    private static void mergeArray2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (k >= 0) {
+            if (j < 0 || (i >= 0 && nums1[i] > nums2[j]))
+                nums1[k--] = nums1[i--];
+            else
+                nums1[k--] = nums2[j--];
+        }
+        System.out.println(Arrays.toString(nums1));
+    }
 }
